78e617a9f26aadd178310e9eda73adc8d6bb4dab
wzr's macbook air <jeffziren.wang@mail.utoronto.cagit config --global user.name ziren1>
2025-12-28 19:21:31 -0500

    Fix Vite base and build input for Vercel

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,10 @@
-import { defineConfig } from 'vite';
+import { defineConfig } from "vite";
 
 export default defineConfig({
-  base: '/3D-art-gallery',
+  base: "./",
   build: {
     rollupOptions: {
-      input: '/main.js',
+      input: "index.html",
     },
   },
 });
